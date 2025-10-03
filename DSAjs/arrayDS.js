2022bd4388a67e325de22fa21b06162271055c3a
Mohamed <mohamedtarekk710@gmail.com>
2025-10-03 16:30:40 +0300

    Leetcode solving array/strings

--- DSAjs/arrayDS.js
+++ DSAjs/arrayDS.js
@@ -1,13 +1,3 @@
-// const strArr = ['a','b','c','d']
-
-// const numArr = [1,2,3,4,5,6]
-
-// const bolArr = [true,false]
-
-// const mix = ['a','tarek',1,true,{b: "b"}]
-
-
-
 // make custom array
 
     class Myarray{
@@ -80,20 +70,20 @@
 
 
     // console.log(mynewarray.get(2))
-    // mynewarray.pop()
+    mynewarray.pop()
 
     // console.log("after pop", mynewarray)
 
-    // mynewarray.shift()
-    // mynewarray.shift()
+    mynewarray.shift()
+    mynewarray.shift()
 
-    // mynewarray.deletebyindex(1)
+    mynewarray.deletebyindex(1)
     mynewarray.insertAt(2, "Grapes")
-    console.log("after :", mynewarray)
-    
+    // console.log("after :", mynewarray)
 
 
-    // string challenge reverse:
+
+    // // string challenge reverse:
     const reverse = (str) => {
         let reversed = "";
 
@@ -103,4 +93,66 @@
         return reversed;
     }
 
-    console.log(reverse("Tarek"))
+    // console.log(reverse("Tarek"))
+
+
+
+    // //string capitalize first char in words challenge
+    const capitalized = (str) => {
+        return str
+        .toLowerCase()
+        .split(" ")
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join("")
+    }
+
+    // console.log(capitalized("my name is cat"))
+
+
+
+    // fizzbuzz
+    const fizzbuzz = (n) => {
+        for(let i = 1; i <= n;i++){
+            if(i % 3 && i % 5){
+                console.log("FizzBuzz");
+            }else if(i % 3){
+                console.log("Fizz");
+            }else if(i % 5){
+                console.log("Buzz");
+            }else{
+                console.log(i);
+            }
+        }
+    }
+
+    // console.log(fizzbuzz(30))
+
+
+
+    //array chunks
+    const chunked = (arr, chunks) => {
+        let chunkedarr = []
+        let index = 0;
+    
+        while(index < arr.length){
+            const chunks = arr.slice(index, index + chunks)
+
+            chunkedarr.push(chunks);
+            index += chunks
+        }
+
+        return chunkedarr;
+    }
+
+
+    //Two sums(ugly code)
+    const twosums = (nums, target) => {
+        for(let i = 0; i < nums.length; i++){
+            for(let j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return [i,j]
+                }
+            }
+        }
+        return [];
+    }
