45833734d40dd73505ab43f4097c39ef5e77112c
Mohamed <mohamedtarekk710@gmail.com>
2025-10-11 13:45:13 +0300

    Implemented full Singly linkedlist in c++

--- linkedList.cpp
+++ linkedList.cpp
@@ -13,7 +13,6 @@ class Linkedlist
 public:
     Node *head;
 
-    // constructor
     Linkedlist()
     {
         head = NULL;
@@ -24,39 +23,42 @@ public:
         return (head == NULL);
     }
 
-    // create:
-    // 1- Create new node
-    // 2- fill data
-    // 3- set next to NULL
-    // 4- head points to new node
-
     void inserFirst(int newvalue)
     {
-        // metkarara fa hanhotha fo2 w khalas
-        Node *newnode = new Node;            // 1
-        newnode->data = newvalue; // 2
+        Node *newnode = new Node;
+        newnode->data = newvalue;
+
         if (isempty())
         {
-            newnode->next = NULL; // 3
-            head = newnode;       // 4
+            newnode->next = NULL;
+            head = newnode;
         }
         else
         {
-            newnode->next = head; // 3
-            head = newnode;       // 4
+            newnode->next = head;
+            head = newnode;
         }
     }
 
     void display()
     {
         Node *temp = head;
+        cout << "\nCurrent Linked List: ";
 
-        // traversing
+        if (temp == NULL)
+        {
+            cout << "[Empty]\n";
+            return;
+        }
+
         while (temp != NULL)
         {
-            cout << temp->data << " \n";
+            cout << temp->data;
+            if (temp->next != NULL)
+                cout << " -> ";
             temp = temp->next;
         }
+        cout << "\n----------------------------------------\n";
     }
 
     int count()
@@ -74,64 +76,157 @@ public:
 
     bool isfound(int key)
     {
-        bool found = false;
         Node *temp = head;
-
         while (temp != NULL)
         {
             if (temp->data == key)
-            {
-                found = true;
-                break;
-            }
+                return true;
             temp = temp->next;
         }
-        return found;
+        return false;
+    }
+
+    void insertBefore(int item, int newvalue)
+    {
+        if (isempty())
+        {
+            inserFirst(newvalue);
+        }
+
+        cout << "\nInserting " << newvalue << " before " << item << "...\n";
+        if (!isfound(item))
+        {
+            cout << "Item " << item << " not found in the list.\n";
+            cout << "----------------------------------------\n";
+            return;
+        }
+
+        Node *temp = head;
+
+        // Case: Insert before head
+        if (head->data == item)
+        {
+            Node *newnode = new Node;
+            newnode->data = newvalue;
+            newnode->next = head;
+            head = newnode;
+            cout << "Inserted " << newvalue << " before head.\n";
+            cout << "----------------------------------------\n";
+            return;
+        }
+
+        // Traverse to node before target
+        while (temp->next != NULL && temp->next->data != item)
+        {
+            temp = temp->next;
+        }
+
+        Node *newnode = new Node;
+        newnode->data = newvalue;
+        newnode->next = temp->next;
+        temp->next = newnode;
+
+        cout << "Inserted " << newvalue << " before " << item << ".\n";
+        cout << "----------------------------------------\n";
+    }
+    void append(int value)
+    {
+
+        if (isempty())
+        {
+            inserFirst(value);
+            return;
+        }
+
+        Node *temp = head;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        Node *newnode = new Node;
+        newnode->data = value;
+        temp->next = newnode;
+        newnode->next = NULL;
+    }
+
+    void Delete(int item)
+    {
+        if (isempty())
+        {
+            cout << "List is empty, no items to delete \n";
+            return;
+        }
+
+        // case 1
+        if (head->data == item)
+        {
+            Node *delptr = head;
+            head = head->next;
+            delete delptr;
+        }
+        else
+        {
+            // case 2
+            Node *delptr = head;
+            Node *prev = NULL;
+
+            while (delptr->data != item)
+            {
+                prev = delptr;
+                delptr = delptr->next;
+            }
+            prev->next = delptr->next;
+            delete delptr;
+        }
     }
 };
 
 int main()
 {
+    cout << "----------------------------\n";
+    cout << "          Tarek app\n";
+    cout << "----------------------------\n";
     Linkedlist list;
 
-    if(list.isempty()){
-        cout << "The List is empty \n";
-    }else{
-        cout << "The list contains" << list.count() << endl;
+    cout << "----------------------------------------\n";
+    if (list.isempty())
+        cout << "The list is currently empty.\n";
+    else
+        cout << "The list contains " << list.count() << " nodes.\n";
+    cout << "----------------------------------------\n";
+
+    int item;
+    for (int i = 1; i <= 3; i++)
+    {
+        cout << "Enter value for node " << i << ": ";
+        cin >> item;
+        list.inserFirst(item);
+        list.display();
     }
 
-        int item;
-        cout << "Enter you Node Data: \n";
-        
-        cin >> item;
-        list.inserFirst(item);
-        list.display();
+    cout << "\nThe list now contains " << list.count() << " nodes.\n";
 
-        cout << "Enter you Node Data: \n";
-        
-        cin >> item;
-        list.inserFirst(item);
-        list.display();
+    cout << "\nEnter item to search: ";
+    cin >> item;
 
-        cout << "Enter you Node Data: \n";
-        
-        cin >> item;
-        list.inserFirst(item);
-        list.display();
+    if (list.isfound(item))
+        cout << "Item " << item << " found in the list.\n";
+    else
+        cout << "Item " << item << " not found.\n";
 
-        cout << "The list contains" << list.count() << endl;
+    cout << "\nEnter [existing item] and [new value] to insert before it:\n";
+    int newvalue;
+    cout << "Existing item: ";
+    cin >> item;
+    cout << "New value: ";
+    cin >> newvalue;
 
+    list.insertBefore(item, newvalue);
+    list.display();
 
-        cout << "Enter item to search \n";
-        cin >> item;
-        if(list.isfound(item)){
-            cout << "Item found";
-        }else{
-            cout << "Item not found";
-        }
-
-        
-    
+    cout << "\nFinal list ready.\n";
+    cout << "----------------------------------------\n";
 
     return 0;
 }
